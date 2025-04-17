812192056d542c0da1ae11fe88f5fb5f1a3d3bb7
Siva Athri <sivaathri@gmail.com>
2025-04-17 08:48:19 +0530

    changes

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -13,6 +13,7 @@ const AdminLayout = () => {
           <li><Link className="text-white" to="/admin/users">Users</Link></li>
           <li><Link className="text-white" to="/login">Logout</Link></li>
         </ul>
+        <p>hello</p>
       </div>
       <div className="flex-grow-1 p-4 bg-light">
         <Outlet />
