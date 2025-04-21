51fe6b96c68a0585bed72304caaff18812c4bc84
Sadhish2001 <sadhish2000@gmail.com>
2025-04-21 19:28:23 +0530

    Api calls 1

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <App />
-  </StrictMode>,
+ 
 )
