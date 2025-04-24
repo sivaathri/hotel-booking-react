5403f8fed44bb340ec41fbd6aec75f3772d7de65
sivaathri <sivaathri24@gmail.com>
2025-04-24 11:37:40 +0530

    Host

--- src/Pages/Host/CreateNewListing.jsx
+++ src/Pages/Host/CreateNewListing.jsx
@@ -205,7 +205,7 @@ const CreateNewListing = () => {
 
   const bedTypes = ['Queen', 'King', 'Twin'];
   const floorTypes = ['Ground Floor', '1st Floor', '2nd Floor', '3rd Floor', '4th Floor', '5th Floor', '6th Floor', '7th Floor', '8th Floor', '9th Floor', '10th Floor'];
-  const bhkTypes = ['1 BHK', '2 BHK', '3 BHK', '4 BHK', '5 BHK', 'Studio'];
+  const bhkTypes = ['1 BHK', '2 BHK', '3 BHK', '4 BHK', '5 BHK', 'Studio','Villa'];
   const roomFacilities = [
     'Private Bathroom',
     'Bathtub',
