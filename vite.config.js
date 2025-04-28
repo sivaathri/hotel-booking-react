0270c97025e34e97eb961ff6e04ddda27ace02b4
sadhish2001 <sadhish2000@gmail.com>
2025-04-28 11:54:28 +0530

    css changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   server: {
-    host: true, // Listen on all addresses
-    port: 5173, // Default Vite port
+    host: true,
+    port: 5173,
   },
 })
