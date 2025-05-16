624cfeb7aef55401cfa49f8987dea80f37565d80
sivaathri <sivaathri24@gmail.com>
2025-05-16 18:03:29 +0530

    our rooms ui designs

--- src/Pages/user/Home.jsx
+++ src/Pages/user/Home.jsx
@@ -107,7 +107,7 @@ const Home = () => {
           data-bs-ride="carousel"
         ></div>
 
-        <div className="mt-50 py-10 px-4 md:px-10 ">
+        <div className="mb-5 md:px-10 ">
           <div className="max-w-7xl mx-auto">
             <div
               className="text-center animate__animated animate__fadeInUp"
