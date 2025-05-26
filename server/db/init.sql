5f3b896c92aef2e4d9d2b297ac9029f5ddf55536
Siva Athri <sivaathri24@gmail.com>
2025-05-26 18:10:41 +0530

    `Added room_bookings table with specified columns and foreign key constraint.`

---
+++ server/db/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE room_bookings (
+    booking_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT, -- optional, if you have a user table
+    property_id INT NOT NULL,
+    room_type VARCHAR(255) NOT NULL,
+    room_number INT,
+    number_of_rooms_Book INT DEFAULT 1,
+    adults INT NOT NULL,
+    children INT DEFAULT 0,
+    check_in_date DATE NOT NULL,
+    check_out_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    payment_status ENUM('Pending', 'Paid', 'Failed') DEFAULT 'Pending',
+    payment_method VARCHAR(100),
+    instant_payment BOOLEAN DEFAULT 0,
+    free_cancellation BOOLEAN DEFAULT 0,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (room_number) REFERENCES room_pricing_availability(id) ON DELETE CASCADE
+);
