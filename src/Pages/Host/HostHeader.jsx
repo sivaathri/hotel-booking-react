2ee9071c86d6106eee0c4246e64a95147ee044bf
Siva Athri <sivaathri24@gmail.com>
2025-05-27 15:46:51 +0530

    `Removed unused notification button from HostHeader component`

--- src/Pages/Host/HostHeader.jsx
+++ src/Pages/Host/HostHeader.jsx
@@ -59,9 +59,9 @@ const HostHeader = () => {
           </Link>
         </div>
         <div className="flex items-center space-x-4">
-          <button className="p-2 text-gray-500 hover:text-gray-700">
+          {/* <button className="p-2 text-gray-500 hover:text-gray-700">
             <Bell className="w-6 h-6" />
-          </button>
+          </button> */}
           <button className="p-2 text-gray-500 hover:text-gray-700">
             <MessageSquare className="w-6 h-6" />
           </button>
